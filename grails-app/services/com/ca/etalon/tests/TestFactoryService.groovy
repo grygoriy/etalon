520c20b15a0caf72ee734253de53f5edfca29572
Grygoriy Mykhalyunyo <grishaster@gmail.com>
2011-09-03 13:20:57 +0300

    optimized using domain ojects

---
+++ grails-app/services/com/ca/etalon/tests/TestFactoryService.groovy
@@ -0,0 +1,68 @@
+package com.ca.etalon.tests
+
+import grails.util.Environment
+import com.ca.etalon.test.motivation.MotivationTest
+import com.ca.etalon.test.motivation.MotivationProcessedQuestion
+import com.ca.etalon.test.lidership.LidershipTest
+import com.ca.etalon.test.yovayshy.YovayshyTest
+import com.ca.etalon.test.yovayshy.YovayshyProcessedQuestion
+import com.ca.etalon.tests.lidership.LidershipQuestion
+import com.ca.etalon.test.lidership.LidersiProcessQuestion
+import com.ca.etalon.tests.motivation.MotivationQuestion
+import com.ca.etalon.test.actuality.ActualTest
+import com.ca.etalon.tests.actualization.ActualQuestion
+import com.ca.etalon.test.actuality.ActualProcessedQuestion
+import com.ca.etalon.test.interestmap.IMTest
+import com.ca.etalon.tests.interestmap.IMQuestion
+import com.ca.etalon.test.interestmap.IMProcessedQuestion
+import com.ca.etalon.tests.yovayshy.YovayshyQuestion
+
+class TestFactoryService {
+
+    static transactional = true
+
+ ActualTest getActualTest() {
+    def questions = ActualQuestion.list()
+    def processedQuestions = questions.collect {new ActualProcessedQuestion(question:it, answer: isTestEnv() ? 1 : 0 )}
+
+    new ActualTest(questions: processedQuestions)
+  }
+
+  IMTest getIMTest() {
+    def questions = IMQuestion.list()
+    def processedQuestions = questions.collect { new IMProcessedQuestion(question: it, answer: isTestEnv() ? 1 : 0)}
+
+    new IMTest(processedQuestions: processedQuestions)
+  }
+
+  YovayshyTest getYovayshyTest() {
+    def questions = YovayshyQuestion.list()
+    def processedQuestions = questions.collect {
+        new YovayshyProcessedQuestion(question:it, answer: isTestEnv() ? 1 : 0)
+    }
+
+    new YovayshyTest(processedQuestions:processedQuestions)
+  }
+
+  LidershipTest getLidershipTest() {
+    def questions = LidershipQuestion.list();
+    def processQuestions = questions.collect {
+        new LidersiProcessQuestion(lidershipQuestion:it, answer: isTestEnv() ? 1 : 0)
+    }
+
+    new LidershipTest(lidersiprocessQuestions:processQuestions)
+  }
+
+  MotivationTest getMotivationTest() {
+    def questions = MotivationQuestion.list()
+    def processedQuestions = questions.collect {
+        new MotivationProcessedQuestion(motivationQuestion:it, answer: isTestEnv() ? 1 : 0)
+    }
+
+    new MotivationTest(motivationProcessedQuestions:processedQuestions)
+  }
+
+   boolean isTestEnv() {
+        Environment.currentEnvironment == Environment.DEVELOPMENT
+    }
+}
